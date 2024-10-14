f4dfaa599f33134feb9421026d49e110bbcf2cee
nordic-mountain <111663675+nordic-mountain@users.noreply.github.com>
2024-10-14 14:14:10 -0700

    Add files via upload

---
+++ yt-dwnl - Copy/scripts/script.js
@@ -0,0 +1,180 @@
+
+let song_list = [
+    [
+        "bouncin",
+        "bxnji",
+        "https://www.adobe.com/express/learn/blog/media_1e8fe58df1cdc1e92b842c67986f84898e4c231a1.png?width=750&format=png&optimize=medium",
+        "music/bouncin.mp3"
+    ],
+    [
+        "Lobster 2.0",
+        "RJ Pastin & WesGhost",
+        "https://lh3.googleusercontent.com/1jvxsJIWten71uH2GqkIyjoVZTRz-uurMG2DnM4GHOYtSAVmaIck_JD0hTtti9IRMbT0bN3ZCS7AGeiY",
+        "music/lobster.mp3",
+    ],
+    [
+        "Burfict!",
+        "JPEGMAFIA x Danny Brown",
+        "https://lh3.googleusercontent.com/Z9SGYBY6uqn9Z0nlMc6BqXaXqnA-YmBpL8KGyq6lJfjsRSRrzvBtvRTyehRKxHEQD6a0fJgVE-XxUbcn",
+        "music/Burfict!.mp3"
+    ],
+    [
+        "i like the way you kiss me",
+        "Artemas",
+        "https://lh3.googleusercontent.com/0tzQNxMXkhmfBS0zaluhQv0vSYbGkXeRKplAXYRkhVZwu1B3RCvbaWcx7OAhLBTfrMmiBLomYroGJoMY",
+        "music/i like the way you kiss me.mp3"
+    ],
+    [
+        "From The Islands",
+        "Jason Derulo, Frozy & Tomo",
+        "https://lh3.googleusercontent.com/SQVAbGFkDeZr-yyL91gXYumTLKB90fCYM9EuwJ8SoqPNC8QxrQzslzZ8rStGwyBGhau9IkWafAGW6bx9",
+        "music/From The Islands.mp3"
+    ],
+    [
+        "PUNK TACTICS",
+        "Joey Valence & Brae",
+        "https://lh3.googleusercontent.com/DTMZyk_KscaP3FfzQVUpQjyc9KRzNtA8KqfKCAFPyws5RajtGtQMKBUNv2f4vzlp7lV-tSmZLXBDIsE",
+        "music/PUNK TACTICS.mp3"
+    ],
+    [
+        "Aces (Phonk)",
+        "e x z",
+        "https://lh3.googleusercontent.com/e-fUyQX2Tv8DVj5PEm2G-OL_gpUhGRj2LfWh4__vSVQ3QUSoNHOS50cxp_zu8ivqL-SDkGBDQjS33oA",
+        "music/Aces (Phonk).mp3"
+    ],
+    [
+        "Surround Sound (feat. 21 Savage & Baby Tate)",
+        "J.I.D",
+        "https://lh3.googleusercontent.com/i_wRso3rywUpOZUp5s2EjysBfH6xYzvdB1eatgelC6SCjK91vkdpHfmOtiMjBZx8gBRuCOekveByyux_",
+        "music/Surround Sound (feat. 21 Savage & Baby Tate).mp3"
+    ],
+    [
+        "Not My Problem (Jersey CLub Remix)",
+        "Laila!",
+        "https://www.adobe.com/express/learn/blog/media_1e8fe58df1cdc1e92b842c67986f84898e4c231a1.png?width=750&format=png&optimize=medium",
+        "music/Not My Problem (Jersey Club Remix).mp3"
+    ],
+    [
+        "Big Dawgs",
+        "Hanumankind",
+        "https://lh3.googleusercontent.com/pg52FEOeoYVpQXERZa3uJ6-UgdZUtJrdN4xi171PUru8qmmt1T77QaGPvT85OOX-XxQDGD7bm_ohJgg",
+        "music/Big Dawgs.mp3"
+    ],
+    [
+        "Limbo",
+        "Freddie Dredd",
+        "https://lh3.googleusercontent.com/V4H6FgocC7C7_MA1n2SsViUMqFaM8gXLn8eghNiCe0iN7dIUdRO0ySFTP4o8XUG5ug4SWKXzDT6BOIQ",
+        "music/Limbo.mp3"
+    ],
+    [
+        "life force",
+        "RJ Pastin",
+        "https://lh3.googleusercontent.com/9jwS8JPj6lsZR09GLWBgC7QAhagWmg3iMv4mGJrp9rSsKtSsYhITn6sAbQ6LUEGHgayJSynEH6-gp3fRtg",
+        "music/life force.mp3"
+    ],
+    [
+        "chad",
+        "RJ Pastin",
+        "https://lh3.googleusercontent.com/DxbIf5R_MgXOVtWzwKrb2c-xOu6qB47CbbX80aR3w4hK1_gq6tXkE1BLHhECJGPCSmqN5lnA4FLIt60G",
+        "music/chad.mp3"
+    ],
+    [
+        "9mm Bang!",
+        "Memphis Cult",
+        "https://lh3.googleusercontent.com/ogr9wzd9VkLnXT71lnpnD4dRIsgsvTbMbhKsTV75fyr9Nf-li5NkLUGxPIznJ-pRG1mhkwTLvNdOBWKr",
+        "music/9MM.mp3"
+    ],
+    [
+        "Million Dollar Baby with RJ Pasin",
+        "Tommy Richman & RJ Pasin",
+        "https://lh3.googleusercontent.com/yaye-gvSo30LmuLiF8uaqBn6Yhjx1R6H92IN93MmeDbrWk-o9WQvHYC2uc0KnvtwtCS3qv2unWnln3iO",
+        "music/Million Dollar Baby with RJ Pasin.mp3"
+    ],
+    [
+        "Pressure (best part)",
+        "Billy Joel",
+        "https://lh3.googleusercontent.com/G6eP-_IZMYG4ffje47uTETOJOMEygZBV1-KbAIl2PT-v5k0PnWyhNcr6FLyP6qHAAcC258yRJn8gtZc",
+        "music/Pressure.mp3"
+    ],
+    [
+        "KEEP UP",
+        "Odetari",
+        "https://lh3.googleusercontent.com/GAKLsl9fPViFdOMQ2GmBh0peBN2jOxByCARFxoTELsuMqY6aWt1PPzao4b-BYBxyJDeVH5KwVEFDewcrDA",
+        "music/KEEP UP.mp3"
+    ],
+];
+
+function shuffle(array) {
+    let currentIndex = array.length;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+};
+
+const coverImage = document.getElementById("cover-image");
+const musicTitle = document.getElementById("music-title");
+const artistTitle = document.getElementById("artist-title");
+const musicPlayer = document.getElementById("audio-player");
+const scrollContainer = document.getElementById("scroll-container");
+const songName = document.getElementsByClassName("songName");
+
+let ranSongList = song_list.sort((a, b) => 0.5 - Math.random()); // randomize playlist order
+
+function setSong(songVar) {
+    songName[0].id = songVar;
+    coverImage.src = ranSongList[songVar][2];
+    musicTitle.innerHTML = ranSongList[songVar][0];
+    artistTitle.innerHTML = ranSongList[songVar][1];
+    musicPlayer.src = ranSongList[songVar][3];
+}
+
+function addSongList(songsData) {
+    let songIter = 0
+    let finalStr = ""
+    songsData.forEach(element => {
+        songIter += 1
+        finalStr+=
+        `
+<div class="scroll-item" id="${songIter-1}">
+    <img style="border-radius: 5px;" height=150 src="${element[2]}" alt="">
+    <div id="preview-song-container">
+        <h2 class="text" >${element[0]}</h1>
+        <h2 class="text" id="artist-title" style="font-size: 20px;">${element[1]}</h2>
+     </div>
+</div>
+        `
+    });
+
+    scrollContainer.innerHTML = finalStr;
+}
+
+addSongList(ranSongList) // add the songs to the playlist
+setSong(0);
+
+musicPlayer.addEventListener("ended", function(event) {
+    if (Number(songName[0].id)+1 > song_list.length) {
+        setSong(0)
+    } else {
+        setSong(Number(songName[0].id)+1)
+    };
+
+    musicPlayer.play();
+});
+
+scrollContainer.addEventListener("click", function(event) {
+    // Check if the clicked element is a div with the class 'clickable-div'
+    if (event.target.classList.contains("scroll-item")) {
+        // Output the id of the clicked div
+        console.log("Clicked div ID:", event.target.id);
+        setSong(event.target.id);
+    }
+});
